4f0d1b78aa4f271792a3984c7d6212c2012ed984
Hema35 <cheershema@gmail.com>
2021-07-22 18:33:23 -0400

    created data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicFormQuestionComponent
   ],
   imports: [
     BrowserModule,
